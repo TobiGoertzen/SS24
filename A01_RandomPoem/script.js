4e78e36310f73749111208d33ecd5f29e73ecbae
TobiGoertzen <149067886+TobiGoertzen@users.noreply.github.com>
2024-03-28 15:11:15 +0100

    add

--- A01_RandomPoem/script.js
+++ A01_RandomPoem/script.js
@@ -1,4 +1,29 @@
 "use strict";
 let subject = "Frank";
-"Claire", "Doug", "";
+"Claire";
+"Doug";
+"Zoe";
+"Lucas";
+"Tom";
+let predicate = "hasst";
+"liebt";
+"tötet";
+"tadelt";
+"bedroht";
+"sucht";
+let object = "Geschenke";
+"Aufmerksamkeit";
+"eine Verschwörung";
+"die Reporter";
+"den Präsidenten";
+"Abgeordnete";
+console.log(subject, predicate, object);
+for (let i = 6; i >= 1; i--) {
+    console.log(i);
+}
+function getVerse(_subject, _predicate, _object) {
+    const verse = "Alohomora";
+    return verse;
+    console.log(getVerse);
+}
 //# sourceMappingURL=script.js.map
--- A01_RandomPoem/script.ts
+++ A01_RandomPoem/script.ts
@@ -1,1 +1,14 @@
-let subject: string = "Frank", "Claire", "Doug", ""
+let subject: string = "Frank"; "Claire"; "Doug"; "Zoe"; "Lucas"; "Tom";
+let predicate: string = "hasst"; "liebt"; "tötet"; "tadelt"; "bedroht"; "sucht";
+let object: string = "Geschenke"; "Aufmerksamkeit"; "eine Verschwörung"; "die Reporter"; "den Präsidenten"; "Abgeordnete";
+console.log(subject, predicate, object);
+
+for (let i = 6; i>= 1; i--) {
+console.log(i);
+}
+
+function getVerse(_subject: string[], _predicate: string[], _object: string[]) {
+const verse = "Alohomora";
+return verse;
+console.log(getVerse);
+}
