f1e7a4befbbb3e17a8264b2ddc20c8c4927a21a9
TobiGoertzen <149067886+TobiGoertzen@users.noreply.github.com>
2024-03-29 21:23:15 +0100

    add

--- A01_RandomPoem/script.js
+++ A01_RandomPoem/script.js
@@ -1,29 +1,40 @@
 "use strict";
-let subject = "Frank";
+let subjects = "Frank";
 "Claire";
 "Doug";
 "Zoe";
 "Lucas";
 "Tom";
-let predicate = "hasst";
+let predicates = "hasst";
 "liebt";
 "tötet";
 "tadelt";
 "bedroht";
 "sucht";
-let object = "Geschenke";
+let objects = "Geschenke";
 "Aufmerksamkeit";
 "eine Verschwörung";
 "die Reporter";
 "den Präsidenten";
 "Abgeordnete";
-console.log(subject, predicate, object);
 for (let i = 6; i >= 1; i--) {
     console.log(i);
+    const verse = getVerse([subjects], [predicates], [objects]);
+    console.log(verse);
 }
-function getVerse(_subject, _predicate, _object) {
-    const verse = "Alohomora";
+function getVerse(_subjects, _predicates, _objects) {
+    const randomSubjectIndex = Math.floor(Math.random() * subjects.length);
+    const removedSubject = subjects.splice(randomSubjectIndex, 1);
+    const randomSubject = removedSubject[0];
+    const randomPredicateIndex = Math.floor(Math.random() * predicates.length);
+    const removedPredicate = predicates.splice(randomPredicateIndex, 1);
+    const randomPredicate = removedPredicate[0];
+    const randomObjectIndex = Math.floor(Math.random() * objects.length);
+    const removedObject = objects.splice(randomObjectIndex, 1);
+    const randomObject = removedObject[0];
+    const verse = { randomSubject, randomPredicate, randomObject };
     return verse;
-    console.log(getVerse);
 }
+const result = getVerse([subjects], [predicates], [objects]);
+console.log(result);
 //# sourceMappingURL=script.js.map
--- A01_RandomPoem/script.ts
+++ A01_RandomPoem/script.ts
@@ -1,14 +1,29 @@
-let subject: string = "Frank"; "Claire"; "Doug"; "Zoe"; "Lucas"; "Tom";
-let predicate: string = "hasst"; "liebt"; "tötet"; "tadelt"; "bedroht"; "sucht";
-let object: string = "Geschenke"; "Aufmerksamkeit"; "eine Verschwörung"; "die Reporter"; "den Präsidenten"; "Abgeordnete";
-console.log(subject, predicate, object);
+let subjects: string = "Frank"; "Claire"; "Doug"; "Zoe"; "Lucas"; "Tom";
+let predicates: string = "hasst"; "liebt"; "tötet"; "tadelt"; "bedroht"; "sucht";
+let objects: string = "Geschenke"; "Aufmerksamkeit"; "eine Verschwörung"; "die Reporter"; "den Präsidenten"; "Abgeordnete";
 
 for (let i = 6; i>= 1; i--) {
-console.log(i);
+    console.log(i);
+    const verse: string = getVerse([subjects], [predicates], [objects]);
+    console.log(verse);
 }
 
-function getVerse(_subject: string[], _predicate: string[], _object: string[]) {
-const verse = "Alohomora";
-return verse;
-console.log(getVerse);
+function getVerse(_subjects: string[], _predicates: string[], _objects: string[]) {
+    const randomSubjectIndex: number = Math.floor(Math.random() * subjects.length);
+    const removedSubject: string[] = subjects.splice(randomSubjectIndex, 1);
+    const randomSubject: string = removedSubject[0];
+
+    const randomPredicateIndex: number = Math.floor(Math.random() * predicates.length);
+    const removedPredicate: string[] = predicates.splice(randomPredicateIndex, 1);
+    const randomPredicate: string = removedPredicate[0];
+
+    const randomObjectIndex: number = Math.floor(Math.random() * objects.length);
+    const removedObject: string[] = objects.splice(randomObjectIndex, 1);
+    const randomObject: string = removedObject[0];
+
+    const verse: string = {randomSubject, randomPredicate, randomObject};
+    return verse;
 }
+
+const result: string = getVerse([subjects], [predicates], [objects]);
+console.log(result);
